c1bc24418b2ea408e700a09c817d8630de6690da
cjeanneau <77149992+cjeanneau@users.noreply.github.com>
2025-06-10 14:44:33 +0200

    update fields

--- app/models/bien_immobilier.py
+++ app/models/bien_immobilier.py
@@ -13,15 +13,11 @@ class BienImmobilierBase(SQLModel):
     code_insee_commune: str = Field(foreign_key="commune.code_insee_commune", max_length=10)
     adresse_normalisee: Optional[str] = Field(default=None)
     code_postal: Optional[str] = Field(default=None, max_length=10)
-    coordonnee_x: Optional[float] = Field(default=None)
-    coordonnee_y: Optional[float] = Field(default=None)
-    geopoint: Optional[str] = Field(default=None)
     reference_cadastrale_parcelle: Optional[str] = Field(default=None, max_length=50)
     type_bien: Optional[str] = Field(default=None, max_length=50, index=True)
     surface_reelle_bati: Optional[int] = Field(default=None, ge=0)
     nombre_pieces_principales: Optional[int] = Field(default=None, ge=0)
     surface_terrain_totale: Optional[int] = Field(default=None, ge=0)
-    date_premiere_apparition: Optional[date] = Field(default=None)
     source_info_principale: Optional[str] = Field(default=None, max_length=10)
 
 class BienImmobilier(BienImmobilierBase, table=True):
@@ -48,15 +44,11 @@ class BienImmobilierUpdate(SQLModel):
     code_insee_commune: Optional[str] = Field(default=None, max_length=10)
     adresse_normalisee: Optional[str] = Field(default=None)
     code_postal: Optional[str] = Field(default=None, max_length=10)
-    coordonnee_x: Optional[float] = Field(default=None)
-    coordonnee_y: Optional[float] = Field(default=None)
-    geopoint: Optional[str] = Field(default=None)
     reference_cadastrale_parcelle: Optional[str] = Field(default=None, max_length=50)
     type_bien: Optional[str] = Field(default=None, max_length=50)
     surface_reelle_bati: Optional[int] = Field(default=None, ge=0)
     nombre_pieces_principales: Optional[int] = Field(default=None, ge=0)
     surface_terrain_totale: Optional[int] = Field(default=None, ge=0)
-    date_premiere_apparition: Optional[date] = Field(default=None)
     source_info_principale: Optional[str] = Field(default=None, max_length=10)
 
 class BienImmobilierReadWithRelations(BienImmobilierRead):
